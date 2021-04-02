b5c37dcb9f1fe032c5a216ca2ca5f85d20f4ec8c
villagef <filwyd123@gmail.com>
2021-04-02 22:03:50 +0200

    styling and small fixes

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ class CookiesAlert {
     this.vendorsAccept = new Array();
     this.API = "https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json";
 
-    this.vendorsContainer = document.querySelector(
-      ".cookie__content--vendors ul"
-    );
+    this.vendorsContainer = document.querySelector(".cookie__content ul");
     this.checkboxes = Array.from(
       document.querySelectorAll(`input[name="check"]:checked`)
     );
@@ -16,6 +14,17 @@ class CookiesAlert {
 
   // create list of vendors
   async manageCookies() {
+    const mainWrapper = document.querySelector(".main__wrapper");
+
+    if (this.getCookie("Vendors") != null) {
+      document.querySelector(".cookie").style.display = "none";
+      mainWrapper.style.filter = "none";
+      document.body.style.overflowY = "scroll";
+    } else {
+      mainWrapper.style.filter = "blur(4px)";
+      document.body.style.overflowY = "hidden";
+    }
+
     this.pullVendors();
 
     this.acceptBtn.addEventListener("click", () => {
@@ -54,8 +63,11 @@ class CookiesAlert {
   // create element with vendor details and checkbox
   createElement({ id, name, policyUrl }) {
     return `
-        <li key=${id}>
-            <span>${name}</span><a href="${policyUrl} target="_blank">link</a><input type="checkbox" value="${id}" name="check">
+        <li key=${id} class="cookie__element">
+            <span>${name}</span>
+            <span>
+            <a href="${policyUrl} target="_blank">policyUrl</a><input type="checkbox" value="${id}" name="check" checked>
+            </span>
         </li>
         `;
   }
@@ -73,21 +85,38 @@ class CookiesAlert {
 
   //handle accept button, save chosen vendors in cookies and hide cookie alert
   handleAcceptBtn() {
-    document.querySelector(".cookie__container").style.display = "none";
-    document.cookie = `Vendors=${this.vendorsAccept}; Expires=${this.handleExpire()}`;
+    document.querySelector(".cookie").style.display = "none";
+    document.querySelector(".main__wrapper").style.filter = "none";
+    document.cookie = `Vendors=${
+      this.vendorsAccept
+    }; Expires=${this.handleExpire()}`;
   }
 
   // handle reject button and hide cookie alert
   handleRejectBtn() {
-    document.querySelector(".cookie__container").style.display = "none";
+    window.location.href = "https://www.pudelek.pl/";
   }
 
+  // handle expire date
   handleExpire() {
     const today = new Date();
     today.setDate(today.getDate() + 1);
     const endDate = today.toString();
 
     return endDate;
+  }
+
+  // get cookie value
+  getCookie(name) {
+    const cookieArr = document.cookie.split(";");
+
+    for (let i = 0; i < cookieArr.length; i++) {
+      let cookiePair = cookieArr[i].split("=");
+      if (name == cookiePair[0].trim()) {
+        return decodeURIComponent(cookiePair[1]);
+      }
+    }
+    return null;
   }
 }
 
