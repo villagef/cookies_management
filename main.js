cdaf6b374121d20a7d5b67a5f67d2741eb0e9418
villagef <filwyd123@gmail.com>
2021-04-02 18:04:04 +0200

    handle checkboxes

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 class CookiesAlert {
   constructor() {
     this.vendors = new Object();
+    this.vendorsAccept = new Array();
     this.API = "https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json";
 
-    this.vendorsContainer = document.querySelector(".vendors__container ul");
+    this.vendorsContainer = document.querySelector(".cookie__content--vendors ul");
     this.checkboxes = document.querySelectorAll(`input[name="check"]:checked`);
+    this.acceptBtn = document.querySelector("#accept");
   }
 
   // create list of vendors
-  async drawList() {
+  async manageCookies() {
     this.pullVendors();
 
+    this.acceptBtn.addEventListener("click", this.handleCheck);
   }
 
   //get all vendors from API and assign to array
@@ -40,11 +43,22 @@ class CookiesAlert {
   createElement({ id, name, policyUrl }) {
     return `
         <li key=${id}>
-            <span>${name}</span><a href="${policyUrl} target="_blank">link</a><input type="checkbox" value="${id}" name="check">
+            <span>${name}</span><a href="${policyUrl} target="_blank">link</a><input type="checkbox" value="${id}" name="check" checked>
         </li>
         `;
+  }
+
+  // handle checkbox for every vendor and assign every checked to array
+  handleCheck() {
+    const checkboxes = document.querySelectorAll(`input[name="check"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+      values.push(checkbox.value);
+    });
+
+    this.vendorsAccept = Object.values(values);
   }
 }
 
 const cookie = new CookiesAlert();
-cookie.drawList();
+cookie.manageCookies();
