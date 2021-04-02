fd1ae20a9003edcedea41e00e8074f66b38fd5c3
villagef <filwyd123@gmail.com>
2021-04-02 19:04:13 +0200

    handle buttons & save in cookies

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ class CookiesAlert {
     this.vendorsAccept = new Array();
     this.API = "https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json";
 
-    this.vendorsContainer = document.querySelector(".cookie__content--vendors ul");
-    this.checkboxes = document.querySelectorAll(`input[name="check"]:checked`);
+    this.vendorsContainer = document.querySelector(
+      ".cookie__content--vendors ul"
+    );
+    this.checkboxes = Array.from(
+      document.querySelectorAll(`input[name="check"]:checked`)
+    );
     this.acceptBtn = document.querySelector("#accept");
+    this.rejectBtn = document.querySelector("#reject");
   }
 
   // create list of vendors
   async manageCookies() {
     this.pullVendors();
 
-    this.acceptBtn.addEventListener("click", this.handleCheck);
+    this.acceptBtn.addEventListener("click", () => {
+      this.handleCheck();
+      this.handleAcceptBtn();
+    });
+
+    this.rejectBtn.addEventListener("click", () => {
+      this.handleRejectBtn();
+    });
   }
 
   //get all vendors from API and assign to array
@@ -43,7 +55,7 @@ class CookiesAlert {
   createElement({ id, name, policyUrl }) {
     return `
         <li key=${id}>
-            <span>${name}</span><a href="${policyUrl} target="_blank">link</a><input type="checkbox" value="${id}" name="check" checked>
+            <span>${name}</span><a href="${policyUrl} target="_blank">link</a><input type="checkbox" value="${id}" name="check">
         </li>
         `;
   }
@@ -57,6 +69,18 @@ class CookiesAlert {
     });
 
     this.vendorsAccept = Object.values(values);
+    console.log("dziala handleCheck");
+  }
+
+  //handle accept button, save chosen vendors in cookies and hide cookie alert
+  handleAcceptBtn() {
+    document.querySelector(".cookie__container").style.display = "none";
+    document.cookie = `Vendors=${this.vendorsAccept.toString()};`;
+  }
+
+  // handle reject button and hide cookie alert
+  handleRejectBtn() {
+    document.querySelector(".cookie__container").style.display = "none";
   }
 }
 
